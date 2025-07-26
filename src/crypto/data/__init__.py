f849cf8b3c70caefae7fbd6cc482fad5b7dc0cac
Pg19g <piotrgalaszek@gmail.com>
2025-07-26 21:26:03 +0200

    Add historical data manager and basic backtesting

---
+++ src/crypto/data/__init__.py
@@ -0,0 +1,6 @@
+"""Data utilities."""
+
+from .collector import DataCollector
+from .history import HistoricalDataManager, OHLCVConfig
+
+__all__ = ["DataCollector", "HistoricalDataManager", "OHLCVConfig"]
